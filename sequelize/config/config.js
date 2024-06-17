08228a0b407ce6cc7eda8ee83a61a1a05d3f5a5c
pardeep889 <pardeep889@hotmail.com>
2024-06-17 11:54:58 +0530

    DB Added

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -6,4 +6,19 @@ development: {
     database: 'tempodb',
     host: 'database-indentifier-tempo.ceuly2qhjpno.us-east-1.rds.amazonaws.com',
     logging: false,
-  }}
+  },
+  production: {
+    dialect: 'postgres',
+    username: process.env.DB_USERNAME || 'tempospace4115',
+    password: process.env.DB_PASSWORD || 'space-41153200',
+    database: process.env.DB_NAME || 'tempospace',
+    host: process.env.DB_HOST || 'tempospace.postgres.database.azure.com',
+    logging: true,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you connect to Azure DB with SSL
+        rejectUnauthorized: false // Azure requires SSL
+      }
+    }
+  }
+}
--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -4,15 +4,21 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+require('dotenv').config();  // Add this line to load the .env file
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+console.log("env", env, "process.env.NODE_ENV", process.env.NODE_ENV)
 
 
 let sequelize = null;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(
+    {
+      ...config
+    }
+  );
 
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
