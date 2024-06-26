04b59ef5ff90f91042dd00f824769cd87de63f39
pardeep889 <pardeep889@hotmail.com>
2024-06-26 15:08:03 +0530

    api added

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,7 +5,7 @@ const BACKEND_URL = import.meta.env.VITE_API_URL;
 
 export const loginApi = async (data) => {
     try {
-      const response = await axios.post(`${BACKEND_URL}/user/login`, data);
+      const response = await axios.post(`/api/user/login`, data);
       console.log("Data",response.data)
       return response.data;
     } catch (error) {
